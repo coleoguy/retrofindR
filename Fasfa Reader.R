1a015485104f8b22dc175aff15f93416c7773a2f
gfischer3 <fischergk3@gmail.com>
2022-06-14 16:25:01 -0500

    Update Fasfa Reader.R

--- Fasfa Reader.R
+++ Fasfa Reader.R
@@ -4,28 +4,36 @@ genome<-read.fasta("Drosophila_melanogaster.BDGP6.32.dna.toplevel.fa")
 load("drosjctlist")
 
 
-eejct_code_list <- vector("list",length(eejct_list))
+eejct_code_list <- list()
+
+gene_names <- c()
 
 for(i in 1:length(eejct_list)){
   cur.table <- eejct_list[[i]]
   curr.seq <- genome[[ which(names(genome) == cur.table$cur.seq[1])]]
   
-
+  
     for(j in 1:nrow(cur.table)){
-      eejct_code_list[[i]][[j]] <- curr.seq[c(cur.table[j,2]:cur.table[j,3],cur.table[j,4]:cur.table[j,5])] 
+      cur.table$curgene.name[j] <- paste0(cur.table$curgene.name[j],"_",j)
       
-     # eejct_code_list[[length(eejct_code_list) + 1]] <- eejct_code
+      eejct_code_list[[length(eejct_code_list) + 1]] <- curr.seq[c(cur.table[j,2]:cur.table[j,3],cur.table[j,4]:cur.table[j,5])] 
+      gene_names <- c(gene_names,cur.table$curgene.name[j])
+     #<- eejct_code 
     }
   
   
 }
 
+eejct_code_list[[1]]
 
+#Check gene names 
+gene_names
 
+#Add names to eejct_code_ist
+names(eejct_code_list)<-gene_names
 
-
-
-
+write.fasta(eejct_code_list,names(eejct_code_list), open="w",as.string = FALSE,
+            nbchar = 60, file.out = "writefastatest.fasta")
 
 
 
