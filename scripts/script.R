9a0fe6df83edc1dcb5c5fe541a304fddce580e8d
Andresdbp <71858994+Andresdbp@users.noreply.github.com>
2023-12-20 10:26:38 -0600

    test run and data

--- scripts/script.R
+++ scripts/script.R
@@ -4,6 +4,7 @@
 
 setwd("~/Documents/GitHub/retrofindR")
 
+#### Create testing data set ####
 exon1 <- sample(c("A","T","C","G"), 120, replace = T)
 exon2 <- sample(c("A","T","C","G"), 75, replace = T)
 exon3 <- sample(c("A","T","C","G"), 48, replace = T)
@@ -26,10 +27,36 @@ query.seq <- paste(unlist(cds.seq), collapse = "")
 sink("data/test.fasta")
 cat(">test\n")
 cat(test.seq)
-file.show("test.fasta")
+file.show("data/test.fasta")
 
 sink("data/query.fasta")
 cat(">query\n")
 cat(query.seq)
-file.show("query.fasta")
+file.show("data/query.fasta")
+#####
+
+
+#### Test Analysis ####
+# create blast local database
+subject.fasta <- "data/test.fasta"
+database.name <- "data/test"
+database.command <- paste("makeblastdb -in", subject.fasta, "-out",
+                          database.name, "-dbtype 'nucl' -hash_index")
+system(command = database.command)
+
+# do blastn
+query.file <- "data/query.fasta"
+output.file <- "data/test_blast.csv"
+blastn.command <- paste("blastn -query", query.file, "-db", database.name,
+                        "-out data/test_blast_no_header.csv -outfmt '6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore'")
+system(blastn.command)
+header <- "qseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqstart\tqend\tsstart\tsend\tevalue\tbitscore"
+header.file <- tempfile(fileext = ".txt")
+writeLines(header, header.file)
+blastn.content <- readLines("data/test_blast_no_header.csv")
+output.content <- c(header, blastn.content)
+writeLines(output.content, output.file)
+
+#read in results
+blast <- read.csv(output.file, sep = "\t")
 
